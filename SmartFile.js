cc3eec1935001a17299d14ac88acfabed2dce994
Atom <121838254+AtomS1101@users.noreply.github.com>
2025-08-22 21:57:11 +0900

    Update SmartFile.js
    minor update

--- SmartFile.js
+++ SmartFile.js
@@ -3,7 +3,7 @@ class SmartFile {
 		const _FM = FileManager.local();
 		this.fm = _FM.isFileStoredIniCloud(module.filename) ? FileManager.iCloud() : _FM;
 		this.libraryPath = this.fm.joinPath(this.fm.libraryDirectory(), filePath);
-		this.extention = filePath.slice(filePath.lastIndexOf(".") + 1).toLowerCase();
+		this.extension = filePath.slice(filePath.lastIndexOf(".") + 1).toLowerCase();
 	}
 	create(content){
 		if (!this.fm.fileExists(this.libraryPath)) {
@@ -13,11 +13,11 @@ class SmartFile {
 	get content(){
 		if (!this.fm.fileExists(this.libraryPath)) return undefined;
 		const Content = this.fm.readString(this.libraryPath);
-		return this.extention == "json"
+		return this.extension === "json"
 			? JSON.parse(Content) : Content;
 	}
 	set content(newValue){
-		this.fm.writeString(this.libraryPath, this.extention == "json"
+		this.fm.writeString(this.libraryPath, this.extension === "json"
 			? JSON.stringify(newValue, null, "\t") : String(newValue)
 		);
 	}
